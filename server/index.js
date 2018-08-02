301bc2d2b85cfb0dddc3f6fb955ab85403c27e8a
TalOmari <talomari592@gmail.com>
2018-08-02 21:25:16 +0300

    deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var app = require('./server.js');
 var port = 3000;
-app.get('/', function (req, res){
-    res.sendFile(path.resolve(__dirname, '../react-client/dist', 'index.html'));
+app.get('*', function (req, res){
+    res.sendFile(path.resolve(__dirname, '../react-client/dist/index.html'));
 })
 app.listen(process.env.PORT || port, function () {
   console.log('listening on port ' + port);
