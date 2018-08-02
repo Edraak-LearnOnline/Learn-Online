ef56c20ba189c372dbc58b7d60aac219b232f358
TalOmari <talomari592@gmail.com>
2018-08-02 21:34:14 +0300

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 var app = require('./server.js');
 var port = 3000;
-app.get('*', function (req, res){
-    res.sendFile(path.resolve(__dirname, '../react-client/dist/index.html'));
-})
+
 app.listen(process.env.PORT || port, function () {
   console.log('listening on port ' + port);
 });
--- server/server.js
+++ server/server.js
@@ -18,12 +18,17 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+
+
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use('/courseController',courseRouter);
 app.use('/adminController',adminRouter);
-
+app.get('*', function (req, res){
+    res.sendFile(path.resolve(__dirname, '../react-client/dist', 'index.html'));
+})
 
 
 module.exports = app;
